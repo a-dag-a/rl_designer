b85aa327516d0bb79bb88f77441274b76bb7e337
TP-Backpacker <balram.pillai@gmail.com>
2024-07-27 16:08:45 -0400

    Update 2

--- designer.py
+++ designer.py
@@ -14,16 +14,83 @@ def loadTemplateNetList(file_path):
 
     return run_description,template_netlist,paramList
 
-netlistPath = 'netlists\\template__CS_Amp.cir'
+# netlistPath = 'netlists\\template__CS_Amp.cir' # Windows
+# netlistPath = 'netlists/template__CS_Amp.cir'
+
+# Test with simpler netlist first
+netlistPath = 'netlists/template__RC_LPF_N3.cir'
+
 run_description,template_netlist,paramList = loadTemplateNetList(netlistPath)
 
 # FIXME: Find a better way to do this
 netlist_simulation = """
 .control
-wrdata results\out.dat all
+ac dec 100 1 1000Meg
+wrdata results/out.dat v(out)
 .endc
 .end
 """
 
-template_netlist.replace('.end',netlist_simulation)
+template_netlist = template_netlist.replace('.end',netlist_simulation)
+trial_values = {
+'R1':1e3,
+'C1':1e-9,
+'R2':10e3,
+'C2':1e-12,
+'R3':10e3,
+'C3':1e-12
+}
 
+# FIXME: Consider making a netlist class, with paramList in the attributres
+# Load the netlist and plant parameters
+def generateNetlist(netlist, paramValues):
+    ''' Plants parameters into netlist'''
+    for _name in paramValues.keys():
+        _searchKey = '{param__' + _name +'}'
+        netlist = netlist.replace(_searchKey,str(paramValues[_name]))
+    return netlist
+
+# Write out the new netlist to see if it simulates fine
+new_netlist = generateNetlist(template_netlist, trial_values)
+with open('new_netlist.cir','w') as f:
+    f.write(new_netlist)
+f.close()
+
+
+# Simulate the netlist
+command_string = f'ngspice -b new_netlist.cir'
+import os
+os.system(command_string)
+
+
+# Load data
+import numpy as np
+data = np.loadtxt('results/out.dat')
+f = data[:,0]
+V = data[:,1]+1j*data[:,2]
+import matplotlib.pyplot as plt
+plt.plot(np.log10(f),20*np.log10(abs(V)))
+plt.show()
+
+V_ideal = 1/(2*np.pi*10e-3)
+
+def computePenalty(V):
+    '''Compute the MSE against the ideal response'''
+    # ideal transfer fcuntion
+    V_ideal = 1/(2*np.pi*10e-3)
+
+    # get mean square error
+    mse = np.mean(abs(V_ideal-V)**2)
+    return mse 
+
+# Can we just use backpropagation to guide the bot?
+
+
+
+
+
+
+
+# Optimizing even a large cascade of two ports 
+# is a differentiable cost function, 
+# so isnt backpropagation a suitable candidate?
